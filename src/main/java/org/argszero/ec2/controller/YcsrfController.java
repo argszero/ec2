c0d71454c22504fb70d4423bc344e50ad18f38f3
argszero <argszero@gmail.com>
2015-03-24 14:30:47 +0800

    myip

--- src/main/java/org/argszero/ec2/controller/YcsrfController.java
+++ src/main/java/org/argszero/ec2/controller/YcsrfController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,13 +14,47 @@ import java.util.Map;
 @Controller
 @RequestMapping("/ycsrf")
 public class YcsrfController {
+    private Map<String, Input> inputMap = new HashMap<String, Input>();
 
-    @RequestMapping( value ="get", method = RequestMethod.GET)
+    @RequestMapping(value = "get", method = RequestMethod.GET)
     @ResponseBody
-    public Map<String, Object> get(String user,String pwd) {
+    public Map<String, Object> get(String user, String pwd) {
         Map<String, Object> result = new HashMap<String, Object>();
-        result.put("input","01:abc");
+        Input input = inputMap.get(user + ":" + pwd);
+        if(input!=null){
+            result.put("input", "01:"+input.input);
+        }
         return result;
+    }
+
+    @RequestMapping(value = "put", method = RequestMethod.GET)
+    @ResponseBody
+    public void get(String user, String pwd, String input) {
+        inputMap.put(user + ":" + pwd, new Input(input, System.currentTimeMillis()));
+    }
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Input> test(String user, String pwd, String input) {
+      return inputMap;
+    }
+
+    private static class Input {
+        private String input;
+        private long time;
+
+        public Input(String input, long time) {
+            this.input = input;
+            this.time = time;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String a = "{\n" +
+                "  \"input\" : \"01:abc\"\n" +
+                "}";
+        System.out.println(a.substring(15, a.length() - 3));
     }
 }
 
